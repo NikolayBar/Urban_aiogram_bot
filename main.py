841704df161472d2a53d185f8743b5786471124f
NikolayBar <nikolbar@yandex.ru>
2024-11-04 23:46:31 +0300

    start edit main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from aiogram import bot, dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
 
+
+with open('UrbanStudentBot.token', 'r', encoding='utf-8') as f:
+    api = f.read().strip()
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
