325a052ed1651e48a7781ecde38c08d99004d403
NikolayBar <nikolbar@yandex.ru>
2024-11-04 23:58:24 +0300

    Homework 13_1

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ with open('UrbanStudentBot.token', 'r', encoding='utf-8') as f:
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
