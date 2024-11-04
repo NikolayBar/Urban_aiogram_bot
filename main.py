7d5c05ef7b83583f96ec0c415dfd1e2174562d27
NikolayBar <nikolbar@yandex.ru>
2024-11-05 02:32:04 +0300

    Homework 13_2

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-
 with open('UrbanStudentBot.token', 'r', encoding='utf-8') as f:
     api = f.read().strip()
 
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    _msg = f'Привет! Я бот помогающий твоему здоровью.'
+    print(message)
+    await message.answer(_msg)
+
 
 @dp.message_handler()
 async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
+    _msg = 'Введите команду /start, чтобы начать общение.'
+    print(message)
+    await message.answer(_msg)
 
 
 if __name__ == '__main__':
