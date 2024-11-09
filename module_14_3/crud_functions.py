c0afb3659cc60f53de3e9fbd5a1f4e54b52922a3
NikolayBar <nikolbar@yandex.ru>
2024-11-09 22:43:05 +0300

    rename database.db, add table Users

--- module_14_3/crud_functions.py
+++ module_14_3/crud_functions.py
@@ -1,6 +1,6 @@
 import sqlite3
 def initiate_db():
-    connection = sqlite3.connect('products.db')
+    connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -10,11 +10,19 @@ def initiate_db():
     description TEXT,
     price INTEGER NOT NULL);''')
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL);""")
+
     connection.commit()
     connection.close()
 
 def get_all_products():
-    connection = sqlite3.connect('products.db')
+    connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM Products")
     all_products = cursor.fetchall()
@@ -24,16 +32,16 @@ def get_all_products():
 
 if __name__ == '__main__':
     initiate_db()
-    connection = sqlite3.connect('products.db')
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM Products")
-    cursor.execute(f"""INSERT INTO Products (title, description, price) 
-    VALUES
-    ('Продукт1', 'Описание 01', 100),
-    ('Продукт2', 'Описание 02', 200),
-    ('Продукт3', 'Описание 03', 300), 
-    ('Продукт4', 'Описание 04', 400);""")
-
-
-    connection.commit()
-    connection.close()
+    # connection = sqlite3.connect('database.db')
+    # cursor = connection.cursor()
+    # cursor.execute("DELETE FROM Products")
+    # cursor.execute(f"""INSERT INTO Products (title, description, price)
+    # VALUES
+    # ('Продукт1', 'Описание 01', 100),
+    # ('Продукт2', 'Описание 02', 200),
+    # ('Продукт3', 'Описание 03', 300),
+    # ('Продукт4', 'Описание 04', 400);""")
+    #
+    #
+    # connection.commit()
+    # connection.close()
